4fb567be60e7a224a2b026f7d65e266b75581496
jaketrent <trent.jake@gmail.com>
2016-05-12 15:10:07 -0600

    menu - overrides in place

--- app-menu-demo/src/main.js
+++ app-menu-demo/src/main.js
@@ -1,7 +1,37 @@
+
+'use strict'
+
 const electron = require('electron')
 
 const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+const name = electron.app.getName()
 
-app.on('ready', _ => {
-  console.log('ready!')
+let template = [
+  {
+    label: name,
+    submenu: [{
+      label: `About ${name}`,
+      role: 'about'
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Quit',
+      accelerator: 'Cmd+Q',
+      click: function () { app.quit() }
+    }]
+  },
+  {
+    label: 'Another',
+    submenu: [{
+      label: 'Sweet'
+    }]
+  }
+]
+
+app.on('ready', function () {
+  new BrowserWindow()
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 })
