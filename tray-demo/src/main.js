238ca98d3f85d130ce7dc7a2ed19d2920e4cf018
jaketrent <trent.jake@gmail.com>
2016-05-13 12:04:33 -0600

    tray - impl and icon

--- tray-demo/src/main.js
+++ tray-demo/src/main.js
@@ -1,7 +1,22 @@
 const electron = require('electron')
+const path = require('path')
 
 const app = electron.app
+const Menu = electron.Menu
+const Tray = electron.Tray
 
 app.on('ready', _ => {
-  console.log('ready!')
+  const tray = new Tray(path.join('src', 'trayIcon.png'))
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Wow',
+      click: _ => console.log('wow')
+    },
+    {
+      label: 'Awesome',
+      click: _ => console.log('awesome')
+    }
+  ])
+  tray.setToolTip('Woweeee')
+  tray.setContextMenu(contextMenu)
 })
